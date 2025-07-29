0bb6d6debd2d9eac5e1a05b24ab6d0e4b2dce84a
KrishnaSharma1109 <Krishnawork118@gmail.com>
2025-07-29 21:09:32 +0530

    add backend url

--- Backend/index.js
+++ Backend/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 
 
 const corsOptions = {
-  origin: 'https://notes-vault-jade.vercel.app', 
+  origin: 'https://notes-vault-delta.vercel.app/', 
   credentials: true,
 };
 
